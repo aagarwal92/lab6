6a5791a31a0f6ac3ce0c6555c3183dd9fff2dada
Ankit <kitilicous@gmail.com>
2014-02-14 02:41:26 -0800

    attempted ajax

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -24,7 +24,12 @@ function addProjectDetails(e) {
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+	var idNumber = projectID.split('project')[1];
+
+	$.get("/project/" +idNumber, function(projectInfo){
+		var details = $("#" + projectID+" .details");
+
+		details.html(projectInfo.summary);
 
 	console.log("User clicked on project " + idNumber);
 }
@@ -34,5 +39,16 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("/palette", function(colorData){
+		var colors = [randomPalette.colors.hex[0], 
+ 			randomPalette.colors.hex[1], 
+ 			randomPalette.colors.hex[2],
+ 			randomPalette.colors.hex[3]];
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 	console.log("User clicked on color button");
 }
